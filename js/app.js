c3048c9f8b79aa1b747779a14086a9fb00525a91
Julian Alejandro Sosa <asosa@webcreek.com>
2020-01-06 06:42:54 -0500

    Reading form data

--- js/app.js
+++ js/app.js
@@ -1,8 +1,43 @@
+// Insurance Constructor
 function Insurance(brand, year, type) {
   this.brand = brand;
   this.year = year;
   this.type = type;
 }
+
+function Interface() {
+}
+
+// EventListeners
+const form = document.getElementById('cotizar-seguro');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Select Brand
+  const brand = document.getElementById('marca');
+  const brandSelected = brand.options[brand.selectedIndex].value;
+
+  // Select Year
+  const year = document.getElementById('anio');
+  const yearSelected = year.options[year.selectedIndex].value;
+
+  // Radio Button
+  const insuranceType = document.querySelector('input[name="tipo"]:checked').value;
+
+  // Interface
+  const interface = new Interface();
+
+  // Check form
+  if (brandSelected === '' || yearSelected === '' || insuranceType === '') {
+    // Instance of Interface showing an error
+    console.log('Data is missing')
+  } else {
+    // Instance of Interface showing the insurance information
+    console.log('All Ok!')
+  }
+
+});
 
 const max = new Date().getFullYear(),
   min = max - 20;
@@ -10,7 +45,6 @@ const max = new Date().getFullYear(),
 const selectYear = document.getElementById('anio');
 
 for (let i = max; i >= min; i--) {
-  console.log(i)
   let option = document.createElement('option');
   option.value = i;
   option.innerHTML = i;
